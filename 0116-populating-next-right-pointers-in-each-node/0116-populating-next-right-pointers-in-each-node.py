03db06bf682701486b1eef9aec0437136512b1c7
Vipsa Kamani <157752900+vive12345@users.noreply.github.com>
2025-12-24 17:44:19 -0700

    Time: 45 ms (87.11%), Space: 18.9 MB (82.12%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -1,4 +1,6 @@
-"""
+
+from collections import deque
+
 # Definition for a Node.
 class Node:
     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
@@ -6,25 +8,50 @@ class Node:
         self.left = left
         self.right = right
         self.next = next
-"""
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root :
-            return root
-        dq = deque([root])
-        while dq:
-            currlevel = len(dq)
-            for i in range(len(dq)):
-                node = dq.popleft()
-                if i<currlevel-1:
-                    node.next = dq[0]
-                else:
-                    node.next = None
-                if node.left:
-                    dq.append(node.left)
-                if node.right:
-                    dq.append(node.right)
+        if not root:
+            return None
+        level_start = root
+        while level_start.left:
+            curr = level_start
+            while curr:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                curr = curr.next
+            level_start = level_start.left
         return root
 
-            
+#     def constructgraph(self, arr):
+#         if not arr:
+#             return None
+#         nodes = [None if v is None else Node(v) for v in arr]
+#         i, j = 0, 1
+#         while j < len(nodes):
+#             if nodes[i]:
+#                 nodes[i].left = nodes[j] if j < len(nodes) else None
+#                 nodes[i].right = nodes[j+1] if j+1 < len(nodes) else None
+#                 j+=2
+#             i+=1
+#         return nodes[0]
+        
+#     def serialize_by_next(self,root):
+#         if not root:
+#             return []
+#         res = []
+#         levelstart = root
+#         while levelstart:
+#             curr = levelstart
+#             while curr:
+#                 res.append(curr.val)
+#                 curr = curr.next
+#             res.append('#')
+#             levelstart = levelstart.left
+#         return res
+  
+# s = Solution()
+# root = s.constructgraph([1,2,3,4,5,6,7,8])
+# s.connect(root)
+# print(s.serialize_by_next(root))
